f47be1e7717287a098a6f85d783139a155ccdb95
Elias Hahn <elias.hahn@mail.de>
2024-03-27 22:23:00 +0100

    fix new project structure

--- types/answerContextType.ts
+++
@@ -1,6 +0,0 @@
-import type { Dispatch, SetStateAction } from "react";
-
-export type AnswerContextType = {
-  setUserAnswers: Dispatch<SetStateAction<Array<string[]>>>;
-  userAnswers: Array<string[]>;
-};
