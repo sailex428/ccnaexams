2ccbd117bfeb5615a783ff67a342f167cd92fb81
sailex <elias.hahn@mail.de>
2024-09-07 17:04:49 +0200

    useProperties read multiple properties in one key

--- frontend/src/components/hooks/useProperties.tsx
+++ frontend/src/components/hooks/useProperties.tsx
@@ -1,6 +1,6 @@
-export function useProperties(key: string[]): string[] {
+export function useProperties(key: string[]): string[][] {
   const propertiesReader = require("properties-reader");
   const properties = propertiesReader("../config/frontend.properties");
 
-  return key.map((k) => properties.get(k));
+  return key.map((k) => properties.get(k).split(","));
 }
