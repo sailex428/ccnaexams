64688390fc307d004107eb62aa49f7202b508c38
sailex <elias.hahn@mail.de>
2024-09-10 20:58:49 +0200

    feat: added better styling to error and notfound page

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className={"notfound"}>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className={"defaultBackground"}>
+      <div className={"notfound"}>
+        <h2>Not Found</h2>
+        <p>Could not find requested resource</p>
+        <Link href="/">Return Home</Link>
+      </div>
     </div>
   );
 }
