834578b3b830e7cbbd82af39b058ee7d2ab34c76
sailex <elias.hahn@mail.de>
2024-09-16 22:21:50 +0200

    feat: added image error and loading handling

--- frontend/src/components/examImage.tsx
+++ frontend/src/components/examImage.tsx
@@ -1,20 +1,26 @@
-import clsx from "clsx";
 import Image from "next/image";
+import styles from "@/styles/components/examImage.module.scss";
 
 const ExamImage = (question: { img: string }) => {
+  const questionImageSrc = "data:image/png;base64," + question.img;
   return (
-    <>
+    <div className={styles.imageWrapper}>
       {question.img ? (
         <Image
-          src={"data:image/png;base64," + question.img}
+          src={questionImageSrc}
           alt={"question image"}
-          className={clsx("image")}
+          className={styles.image}
           fill={true}
+          onError={(e) => {
+            e.currentTarget.src = "/images/fallback.png";
+          }}
+          blurDataURL={questionImageSrc}
+          placeholder={"blur"}
         />
       ) : (
         <></>
       )}
-    </>
+    </div>
   );
 };
 
